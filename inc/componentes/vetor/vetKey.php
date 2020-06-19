5c27e5fdc8273261ceac81a42d14c2af08b7cbc9
Lucas Romero <lucas.romero@doutoresdaweb.com.br>
2020-06-19 19:31:51 -0300

    vetor

---
+++ inc/componentes/vetor/vetKey.php
@@ -0,0 +1,41 @@
+<?php
+
+//criando array
+$vetKey = array();
+
+$vetKey[0] = array(
+'name' 		=> 'Portaria',
+'content' 	=> 'É como o cartão de visitas do local, devendo sempre manter um clima cordial, respeitoso e profissional em seu posto de trabalho. Este profissional tem como suas principais atividades, receber e distribuir correspondências destinadas aos condôminos, transmitir e cumprir ordens do zelador ou síndico, fiscalizar a entrada de pessoas e veículos, dar conhecimento ao zelador ou síndico de todas as reclamações e sugestões que ocorrerem durante seu turno.',
+'cover'		=> 'portaria'
+
+);
+
+$vetKey[1] = array(
+'name' 		=> 'Limpeza',
+'content' 	=> 'O profissional designado auxiliar de limpeza é aquele que se dedica profissionalmente ao asseio de locais como condomínios residenciais, empresas, indústrias, etc. Tem como principais atividades a conservação e limpeza de edifícios e outros locais, além de efetuar a remoção de lixo, fazer lavagem de vidraças e persianas, desentupir ralos e pias e executar outras tarefas da mesma natureza, dentre outros. Nossos colaboradores são instruídos a sempre fazer uso de EPIs (equipamento de proteção individual) conforme cada necessidade, além de atentar sempre para o correto uso de produtos de limpeza e equipamentos.',
+'cover'		=> 'limpeza'
+
+);
+
+$vetKey[2] = array(
+'name' 		=> 'Jardinagem',
+'content' 	=> 'A manutenção e conservação de áreas ajardinadas é uma das atividades mais corriqueiras em condomínios residenciais e empresas. São locais que geram sensação de conforto e bem-estar aos seus frequentadores, e devem ter um cronograma bem definido de cuidados. Nossa equipe de jardinagem é treinada e capacitada para a realização destes serviços, que incluem: poda de diferentes espécies arbóreas, limpeza, controle de pragas e doenças, adubação e fertilização do solo.',
+'cover'		=> 'jardinagem'
+
+);
+
+$vetKey[3] = array(
+'name' 		=> 'Zeladoria',
+'content' 	=> 'É o funcionário que trabalha na área de limpeza e manutenção completa dos edifícios. O Zelador é o profissional a quem compete a ter contato direto com a administração do condomínio, agindo como preposto do síndico, transmitir as ordens dos superiores e fiscalizar seu cumprimento, fiscalizar as áreas de uso comum, verificar o funcionamento das instalações hidráulicas e elétricas, assim como aparelhos de uso comum, além de zelar pela observância da disciplina no edifício, de acordo com seu regimento interno, e muito mais.',
+'cover'		=> 'limpeza'
+
+);
+
+$vetKey[4] = array(
+'name' 		=> 'Manutenção Predial',
+'content' 	=> 'O profissional denominado auxiliar de manutenção deve estar preparado para detectar problemas, fazer o diagnóstico e solucioná-los. Quando necessário, deve solicitar orçamentos e acompanhar o reparo.',
+'cover'		=> 'predial'
+
+);
+
+?>
